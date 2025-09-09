32848e759f7c4590cc1080ab5158ac3efa6da1ee
Pimenta15 <jpmp150404@gmail.com>
2025-09-09 16:39:42 -0300

    first commit

---
+++ src/main/java/com/pethealth/pethealth/PethealthApplication.java
@@ -0,0 +1,13 @@
+package com.pethealth.pethealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PethealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PethealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pethealth/pethealth/PethealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pethealth.pethealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PethealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
